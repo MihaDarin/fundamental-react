9546d5bbb8594693b6dda71361906aa7afd6efe4
Mike <mihaildaryin@gmail.com>
2022-06-29 22:36:11 +0300

    Добавлена домашняя страница приложения, стилизованы пункты меню

--- src/components/AppRouter/AppRouter.js
+++ src/components/AppRouter/AppRouter.js
@@ -4,6 +4,7 @@ import { About } from "../../Pages/About";
 import Posts from "../../Pages/Posts";
 import { Error } from "../../Pages/Error";
 import { PostIdPage } from "../../Pages/PostIdPage";
+import { HomePage } from "../../Pages/HomePage";
 export const AppRouter = () => {
   return (
     <Routes>
@@ -11,6 +12,7 @@ export const AppRouter = () => {
       <Route path="/posts" element={<Posts />} />
       <Route path="/posts/:id" element={<PostIdPage />} />
       <Route path="*" element={<Error />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 };
--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../../App.css";
+import style from "./Navbar.module.css";
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__items ">
-        <Link to="/about">О приложении</Link>
-        <Link to="/posts">Посты</Link>
+    <div className={style.navbar}>
+      <div className={style.navbar__items}>
+        <Link className={style.navbar__item} to="/about">
+          О приложении
+        </Link>
+        <Link className={style.navbar__item} to="/posts">
+          Посты
+        </Link>
+        <Link className={style.navbar__item} to="/">
+          Домашняя страница
+        </Link>
       </div>
     </div>
   );
