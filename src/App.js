e207013862ab1412d2ebd066098253ac596d1a25
Mike <mihaildaryin@gmail.com>
2022-07-15 21:55:27 +0300

    создан кастомный хук usePosts для фильтрации и сортировки постов

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { usePosts } from "./Components/hooks/usePosts";
 import { PostFilter } from "./Components/PostFilter/PostFilter";
 import { PostForm } from "./Components/PostForm/PostForm";
 // import { Post } from "./Components/Post/Post";
@@ -30,20 +31,8 @@ function App() {
   };
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
